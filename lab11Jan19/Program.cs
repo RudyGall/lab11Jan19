7530cf712a4c4bd98ebc79f7302be9700756d320
Rudy Gall <rudyegall@gmail.com>
2019-01-29 17:40:24 -0500

    Initial commit

---
+++ lab11Jan19/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11Jan19
+{
+    class Program
+    {
+        public static bool Continue()
+        {
+            Console.WriteLine("\nWould you like to run the application again? (Y or N)");
+            string input = Console.ReadLine().ToLower();
+            input = input.ToLower();
+            bool goOn;
+            if (input == "y")
+            {
+                goOn = true;
+            }
+            else if (input == "n")
+            {
+                Console.WriteLine("Thank you, Goodbye!");
+                Console.ReadLine();
+                goOn = false;
+            }
+            else
+            {
+                Console.WriteLine("I don't understand that, let's try again");
+                goOn = Continue();
+            }
+            return goOn;
+        }
+        static void Main(string[] args)
+        {
+            bool runP = true;
+            while (runP == true)
+            {
+                Movie SciFi1 = new Movie("SciFi1", 1);
+                Movie SciFi2 = new Movie("SciFi2", 1);
+                Movie SciFi3 = new Movie("SciFi3", 1);
+                Movie Ani1 = new Movie("Ani1", 2);
+                Movie Ani2 = new Movie("Ani2", 2);
+                Movie Dra1 = new Movie("Dra1", 3);
+                Movie Dra2 = new Movie("Dra2", 3);
+                Movie Hor1 = new Movie("Hor1", 4);
+                Movie Hor2 = new Movie("Hor2", 4);
+                Movie Hor3 = new Movie("Hor3", 4);
+
+                List<Movie> movieList = new List<Movie>();
+                movieList.Add(SciFi1);
+                movieList.Add(SciFi2);
+                movieList.Add(SciFi3);
+                movieList.Add(Ani1);
+                movieList.Add(Ani2);
+                movieList.Add(Dra1);
+                movieList.Add(Dra2);
+                movieList.Add(Hor1);
+                movieList.Add(Hor2);
+                movieList.Add(Hor3);
+
+                int movieCt = movieList.Count;
+                Console.WriteLine("Welcome to the Movie List Application!");
+                Console.WriteLine("\nThere are {0} movies in this list.", movieCt);
+                Console.WriteLine("Select a category: ");
+                Console.WriteLine("1: SciFi");
+                Console.WriteLine("2: Animated");
+                Console.WriteLine("3: Drama");
+                Console.WriteLine("4: Horror");
+                Console.WriteLine("");
+                Console.Write("enter a number 1-4): ");
+                int.TryParse(Console.ReadLine(), out int userInt);
+
+                foreach (Movie m in movieList)
+                {
+                    switch (userInt)
+                    {
+                        case 1:
+                            m.PrintSci();
+                            break;
+                        case 2:
+                            m.PrintAni();
+                            break;
+                        case 3:
+                            m.PrintDra();
+                            break;
+                        case 4:
+                            m.PrintHor();
+                            break;
+                    }
+                }
+                runP = Continue();
+            }
+
+        }
+    }
+}
---
+++ lab11Jan19/movie.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab11Jan19
+{
+    class Movie
+    {
+        static string sci = "Scifi";
+        static string ani = "Animated";
+        static string dra = "Drama";
+        static string hor = "Horror";
+
+        public Movie()
+        {
+            Title = "";
+            Category = "";
+        }
+        public Movie(string t, int c)
+        {
+            Title = t;
+
+            switch (c)
+            {
+                case 1:
+                    Category = sci;
+                    break;
+                case 2:
+                    Category = ani;
+                    break;
+                case 3:
+                    Category = dra;
+                    break;
+                case 4:
+                    Category = hor;
+                    break;
+            }
+
+        }
+        public string Title { get; set; }
+        public string Category { get; set; }
+
+        public void PrintAni()
+        {
+            if (Category == ani)
+            {
+                Console.WriteLine(Title);
+            }
+        }
+        public void PrintDra()
+        {
+            if (Category == dra)
+            {
+                Console.WriteLine(Title);
+            }
+        }
+        public void PrintHor()
+        {
+            if (Category == hor)
+            {
+                Console.WriteLine(Title);
+            }
+
+        }
+        public void PrintSci()
+        {
+            if (Category == sci)
+            {
+                Console.WriteLine(Title);
+            }
+        }
+
+    }
+
+}
